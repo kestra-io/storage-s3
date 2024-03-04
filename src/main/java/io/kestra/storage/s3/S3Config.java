2837bcceafa68240fff31c79feeca5b134f6e814
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-03-04 23:02:39 +0100

    feat: add support for StsAssumeRoleCredentialsProvider
    This commit adds support for StsAssumeRoleCredentialsProvider allowing
    to assume an IAM role and obtain temporary credentials.

--- src/main/java/io/kestra/storage/s3/S3Config.java
+++ src/main/java/io/kestra/storage/s3/S3Config.java
@@ -1,21 +1,46 @@
 package io.kestra.storage.s3;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.bind.annotation.Bindable;
+import jakarta.annotation.Nullable;
 import jakarta.inject.Singleton;
-import lombok.Data;
-import lombok.Getter;
 
+import java.time.Duration;
+
+/**
+ * The AWS S3 configuration.
+ *
+ * @param bucket                 The S3 bucket name.
+ * @param region                 The S3 region.
+ * @param endpoint               The S3 endpoint.
+ * @param accessKey              The AWS Access Key ID
+ * @param secretKey              The AWS Secret Key.
+ * @param stsRoleArn             The AWS STS Role.
+ * @param stsRoleExternalId      The AWS STS External ID.
+ * @param stsRoleSessionName     The AWS STS Session name.
+ * @param stsRoleSessionDuration The AWS STS Session duration.
+ * @param stsEndpointOverride    The AWS STS Endpoint.
+ */
 @Singleton
-@Data
+@S3StorageEnabled
 @ConfigurationProperties("kestra.storage.s3")
-public class S3Config {
-	String bucket;
+public record S3Config(
+    String bucket,
 
-	String region;
+    String region,
 
-	String endpoint;
+    @Nullable String endpoint,
 
-	String accessKey;
+    @Nullable String accessKey,
 
-	String secretKey;
+    @Nullable String secretKey,
+    @Nullable String stsRoleArn,
+    @Nullable String stsRoleExternalId,
+    @Nullable String stsRoleSessionName,
+    @Nullable String stsEndpointOverride,
+
+    @Bindable(defaultValue = "15m")
+    Duration stsRoleSessionDuration
+) {
+
 }
