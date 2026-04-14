dc30ed764534f584a802746cd8eee88388ccab63
François Delbrayelle <fdelbrayelle@kestra.io>
2026-04-14 09:35:26 +0200

    fix: remove unused Objects import from S3FilesStorage

--- src/main/java/io/kestra/storage/s3/S3FilesStorage.java
+++ src/main/java/io/kestra/storage/s3/S3FilesStorage.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
